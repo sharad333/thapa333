380899deed747ece8b11e69a114fc8f291a3d7b6
SHARAD <thapasharad333@gmail.com>
2018-02-28 21:16:33 +0530

    okkk 555

--- thapa3/ViewController.m
+++ thapa3/ViewController.m
@@ -19,7 +19,7 @@
     
     NSLog(@"okk 2222");
     NSLog(@"okk 111");
-    NSLog(@"hiii 4444");
+    NSLog(@"hiii 333333");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
